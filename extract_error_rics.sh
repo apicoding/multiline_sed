96f83b4f621d60d0e8d6585bb5fa9a1fa9846dab
apicoding <apicodingprj@gmail.com>
2022-09-18 16:53:34 +0200

    Trying to extract error RICs from multi line pattern

--- extract_error_rics.sh
+++ extract_error_rics.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # https://www.gnu.org/software/sed/manual/html_node/Text-search-across-multiple-lines.html
+# https://www.gnu.org/software/sed/manual/html_node/Multiline-techniques.html#Multiline-techniques
 
 # = Print the current input line number (with a trailing newline)
 
-sed -En '{N; /\bArgumentException.*RIC (.*) while subscribing\b/{=;p} ; D}' swingst.log
+sed -En '/ArgumentException.*while subscribing/{H;$!d} ; x  ; s/^.*RIC (.*) while subscribing.*$/\1/p' swingst.log
